24ab9e3821f0db367f54d2eecb4c781904001d3c
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-04-29 20:00:59 -0300

    ✨ feat [auth-service]: Criei a logica de login já utilizando o 'jwt Token'

---
+++ services/auth-service/controllers/login.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vidinine-ecommerce/aut-service/services"
+)
+
+func LoginHandler(c *gin.Context) {
+	var creds struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindBodyWithJSON(&creds); err != nil {
+		c.JSON(400, gin.H{"error": "Dados inválidos"})
+		return
+	}
+
+	token, err := services.Login(creds.Email, creds.Password)
+	switch err {
+	case services.ErrUserNotFound:
+		c.JSON(http.StatusNotFound, gin.H{ // 404 ou 401
+			"error":   "usuario_nao_encontrado",
+			"message": "Nenhuma conta com este email foi encontrada",
+		})
+		return
+	case services.ErrWrongPassword:
+		c.JSON(http.StatusUnauthorized, gin.H{ // 401
+			"error":   "senha_incorreta",
+			"message": "A senha está incorreta",
+		})
+		return
+	case nil:
+		// Token gerado com sucesso
+	default:
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":   "erro_interno",
+			"message": "Ocorreu um erro inesperado",
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{"token": token})
+}
---
+++ services/auth-service/services/login.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"errors"
+
+	"github.com/vidinine-ecommerce/aut-service/config"
+	"github.com/vidinine-ecommerce/aut-service/models"
+	"github.com/vidinine-ecommerce/aut-service/utils"
+	"gorm.io/gorm"
+)
+
+var (
+	ErrUserNotFound  = errors.New("usuário não encontrado")
+	ErrWrongPassword = errors.New("senha incorreta")
+)
+
+func Login(email, password string) (string, error) {
+	var user models.User
+
+	if err := config.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return "", ErrUserNotFound
+		}
+		return "", err
+	}
+
+	if !utils.CheckPasswordHash(password, user.Password) {
+		return "", ErrWrongPassword
+	}
+
+	token, err := utils.GenerateToken(user.ID)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
