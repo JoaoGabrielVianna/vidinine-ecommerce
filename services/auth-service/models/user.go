46d6be6b23d7bb14d9e2ddfa3c6e199109c0fe17
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-04-30 20:35:46 -0300

    ✨ feat [auth-service]: Adicionando o  `Role`  no `model` do `User`

--- services/auth-service/models/user.go
+++ services/auth-service/models/user.go
@@ -12,9 +12,17 @@ type User struct {
 	Name     string `json:"name" gorm:"not null"`              // 📝 Name of the user
 	Email    string `json:"email" gorm:"not null;unique"`      // 📧 Email of the user
 	Password string `json:"password" gorm:"not null;size:255"` // 🔒 Password of the user
+	Role     Role   `json:"role" gorm:"type:text;check:role IN ('admin','user');default:'user'"`
 }
 
 type UpdateUser struct {
 	Name  string `json:"name" binding:"omitempty,min=2"`  // Obrigatório se enviado
 	Email string `json:"email" binding:"omitempty,email"` // Valida formato de email
 }
+
+type Role string
+
+const (
+	AdminRole Role = "admin"
+	UserRole  Role = "user"
+)
